bafc879bfbdb3188dcad09128808d3d64a6b24d5
pollob <pollob124803@gmail.com>
2023-07-09 00:12:05 +0600

    upload done

---
+++ assets/js/data.js
@@ -0,0 +1,193 @@
+const posts = [
+    {
+        id: 1,
+        image: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/347392033_1740071436407498_843231641689313823_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeHWZkXdF-d4TUTbS7Vf8Tq86tDEkE13hnzq0MSQTXeGfFb0dNXTEtaUWCRvd0Xtj24qWnv8sYjUjs7Kgvg-aQEt&_nc_ohc=_CsLO2e0KREAX_ouaks&_nc_ht=scontent.fdac31-1.fna&oh=00_AfB_zO9kLjT92gPnClDKL0bhQpeivPUqdtcrp3G_yqGPMw&oe=64AC56D1",
+        contet:"It's party time and you are all welcome 🙏",
+        like: 300,
+        commet : 20,
+        timeAgo:"30 Min",
+        author :{
+            name: "Mamadou Saidou Jallow",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/358083984_1740045999743375_5095847655953916133_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeE-DjPjP6xmbw57lX4Qkk2FGxXiJI98w1QbFeIkj3zDVJHKbiSsyf5N6KLxJHftBQ5E-efQ3yJMQjxN4NvWDu8p&_nc_ohc=lbpiEKcGS-EAX-xEdYU&_nc_ht=scontent.fdac31-1.fna&oh=00_AfAJDBAjgp-bW1mUjH-BUITv0v--WM4bXIKthAfbMVjcGg&oe=64ABE2B0",
+        },
+
+        
+
+
+    },
+
+
+    {
+        id: 2,
+        image: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/358456475_832807034865224_8534350311601441207_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEEMy5dwXR_OG53K-SDfppJCSO4uT4aHiIJI7i5PhoeIn8NhbZhzB0GOjvnej5INRr0SGP-vdR6smZeWfWZ_PoX&_nc_ohc=KbWHlaffw2AAX-l3tOC&_nc_ht=scontent.fdac31-1.fna&oh=00_AfC-dHSbLVlK99VUwW4LHv7mMQv_Z55LLX1APabM2D2WEQ&oe=64ACDC65",
+        contet:`From 2003, we took our first steps together at Under 15 level playing for Bangladesh's national team and shared our dreams and goals together to build a formidable bond and friendship over 20 years. 
+        We, among many others, sacrificed a lot to take Bangladesh cricket to the next level and your passion and aggression has helped inspire many more.
+        We believed and relied  on each other's strengths to achieve a common goal - to win for our country.
+        Your runs and records speak for themselves and we, as team mates, are immensely proud of everything you have accomplished as a player.
+        It's going to be strange to not be in the middle with you anymore...but your fire will be inside us all when we step into every battle.
+        May you continue to cross boundaries and hit sixes in life and enjoy new moments with your loved ones.`,
+        like: "30k",
+        commet :" 2k",
+        timeAgo:"10 Min",
+        author :{
+            name: "Shakib Al Hasan",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/345650734_1938987979801695_5251002386057623293_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeE08A0Rw7hA-65I98RpbK1CFzqRUb0idbYXOpFRvSJ1tvD1KaMmv6nDdazr_JBgGIyBO0Ik97HJ6rxC9z8zeZfH&_nc_ohc=dWB0ayV1lusAX87BnEJ&_nc_ht=scontent.fdac31-1.fna&oh=00_AfCAIbfzqbSkyVOW_G5ArV-SM3QH6LI2zceKy_-B3gUWCQ&oe=64AC00EE",
+        },
+
+        
+
+
+    },
+
+      
+    {
+        id: 3,
+        image: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/358696334_285941754094153_5382747083694657884_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGl_Yo9nWAxBE4Y9LAiE-X_aXDUtTvgZSNpcNS1O-BlI0yh_7fSitfZ_4H-mknQ2In6_vdP_Y-3O1jvhMlIPcgZ&_nc_ohc=Omswzyw5aWEAX_dIhyR&_nc_ht=scontent.fdac31-1.fna&oh=00_AfD0jw-oNXkoZMQTQGuHPqOmHMDU00uPr43URQboGN-Mnw&oe=64AD587B",
+        contet:`আর ও কিছুক্ষন থেকে যাও,যেও না এখনি! 🤍`,
+        like: 500,
+        commet :" 2k",
+        timeAgo:"40 Min",
+        author :{
+            name: "Sorry Priyo - সরি প্রিয়",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/275673024_127825429811400_74803478024524733_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGbOVgkkp2lNIeVJKU72pWiFIMj5foxMZ4UgyPl-jExnk8ODmO7tl-R4PQrpFk1Owy44XFWsjob94QwM-3-RfO6&_nc_ohc=eeB-uwXA3gwAX_pzm4s&_nc_ht=scontent.fdac31-1.fna&oh=00_AfA3ZU-5nzwHoLeRqybIeD0BQ0m5u9WrCB8n9uOjc59R2w&oe=64ACE0F7",
+        },
+
+        
+
+
+    },
+
+
+    {
+        id: 4,
+        image: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/358680258_121968977610635_8125977201130690923_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeHxWPRAQPuqCMNYhZzU71msBrfVxbSqYgAGt9XFtKpiAMTAtcXNiIvW8li1eoifNZUKuLFZ7snKoWarmWxBpREs&_nc_ohc=HzdzfhnWfyQAX_l1PIA&_nc_ht=scontent.fdac31-1.fna&oh=00_AfDDtJX-hn-ny0tZeFHLQ8pp-Fi6B6siQJ7tOUfevHWVAg&oe=64AD8BB8",
+        contet:``,
+        like:"2k",
+        commet :300,
+        timeAgo:"2 Min",
+        author :{
+            name: "আল-মুমিন",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/356244998_111987715275428_8368262349861523995_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHgUwtDZ3RHfeIx10qDmINudpat5O8_0v92lq3k7z_S_9Vt2geZlsVtpfh2jWXS2GF8FfLVYdQEEDaQH0OSsifJ&_nc_ohc=FgQg_K1sKjAAX-bqIo5&_nc_ht=scontent.fdac31-1.fna&oh=00_AfCBGYhV8A4K2zHl2lySq9QzvVg4sS25g0ZzkEP3tZtPqQ&oe=64AC0C8B",
+        },
+
+        
+
+
+    },
+
+
+    {
+        id: 5,
+        image: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/358113355_162705693481155_9181406314714713132_n.jpg?stp=dst-jpg_p843x403&_nc_cat=100&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeF2TRyQJGpikAoog06BHEXAi3JiQCbZYteLcmJAJtli1whL70P17sn1BegdqXTlgjSSC8XBLl1wwcKqkaZooRUg&_nc_ohc=Y5mJdNXVSJgAX-qf0Jl&_nc_ht=scontent.fdac31-1.fna&oh=00_AfBu4_5AsyGGCoZmPeBT0uxXvfs8FFBpSL3owsKuYvkpLw&oe=64AD30F8",
+        contet:`ঠিক আছে 🖤`,
+        like:"5k",
+        commet :"1k",
+        timeAgo:"2 Min",
+        author :{
+            name: "Hanifa Khatun",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t1.6435-9/180731800_286970049749913_897548191187272598_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF0fiR_eCmwGjyRPm4T6QGQUtIAJW0Jae9S0gAlbQlp72gKq2_AIviYkEcOsuHHNbR3xS82ykRy-FR0q8OhREIM&_nc_ohc=e_Blaje2OBEAX8HDidw&_nc_ht=scontent.fdac31-1.fna&oh=00_AfDbz1GxwPyJUOgUaI82WEWrxREY7WO8QVI6Xj1ITJX7Gg&oe=64CF6A36",
+        },
+
+        
+
+
+    },
+
+    {
+        id: 6,
+        image: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/357735822_285518277469834_5720240491648819707_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFpcdaQ8CGE_71cdJ2ipZNE6zLbZEeeMxXrMttkR54zFfhUVxM_CDZ-5i-0rbbbfL_xJTuOsRaqZMlW7JG4oGKB&_nc_ohc=UUJhoVa1njYAX9m0cLN&_nc_ht=scontent.fdac31-1.fna&oh=00_AfAHvE3FLLEn4Swlj8iYlRkoeajhVnpxZtFRc_TVVf7puw&oe=64ABF395",
+        contet:`I believe in--বেলি ফুল Supremacy!! 🤍`,
+        like: 500,
+        commet :40,
+        timeAgo:"40 Min",
+        author :{
+            name: "Sorry Priyo - সরি প্রিয়",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/275673024_127825429811400_74803478024524733_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGbOVgkkp2lNIeVJKU72pWiFIMj5foxMZ4UgyPl-jExnk8ODmO7tl-R4PQrpFk1Owy44XFWsjob94QwM-3-RfO6&_nc_ohc=eeB-uwXA3gwAX_pzm4s&_nc_ht=scontent.fdac31-1.fna&oh=00_AfA3ZU-5nzwHoLeRqybIeD0BQ0m5u9WrCB8n9uOjc59R2w&oe=64ACE0F7",
+        },
+
+        
+
+    },
+
+
+     
+    {
+        id: 7,
+        image: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/358103816_662178675937789_417561590614526327_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGFTqFTl-jaLSwxL3l1gEc78QFBSmXy3G3xAUFKZfLcbW1Oo56W37GaDzIaR28ZAeHeR39TOlMzfUhnv8sPVB2M&_nc_ohc=RwkLnqS2wLoAX__-kmD&_nc_ht=scontent.fdac31-1.fna&oh=00_AfCLm7w_0CuBDc3p1WFWU3znctbIYgLm4lqxarX3IaGS9w&oe=64AC6B27",
+        contet:`লোকটার চোখে ২ দিন আগেই ছিলো বিশ্বকাপ জয়ের স্বপ্ন; আর আজ!!😭`,
+        like: 300,
+        commet :" 1k",
+        timeAgo:"50 Min",
+        author :{
+            name: "চাটগাঁইয়া নবাব",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/358472723_662186755936981_2583490249882835014_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGN9HA4CdltivWHKXyZEsMxxkZZkQBAp4DGRlmRAECngJ4jqG-7U32yVG5Am1XFBTCUXNDXrU837R0cwIvVAUUm&_nc_ohc=d69sgo7QjwcAX-NLkqL&_nc_ht=scontent.fdac31-1.fna&oh=00_AfDNQLGz6nALiknMV2yT0sRzz3rj8gSxu_SeLj5J8uBpwA&oe=64AC481B",
+        },
+
+        
+
+
+    },
+
+
+    {
+        id: 8,
+        image: "",
+        contet:`মানুষের অর্ধেক জীবন কাটে মানুষ চিনতে চিনতে, বাকী অর্ধেক কাটে অভিমানে।🌸❤️`,
+        like: 300,
+        commet :" 1k",
+        timeAgo:"40 Min",
+        author :{
+            name: "Avoid",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/307309017_406474234971752_6364205639120603119_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHStd9CZfu5wYkuYtZbgJgLuzQLDUSsrXm7NAsNRKyteZiPILv9Zs5lzurErD2TdX5pii2Gbis3RVMl2w0drILY&_nc_ohc=PlZu4FT153QAX99Lt34&_nc_ht=scontent.fdac31-1.fna&oh=00_AfCjFPM7Vqrlk232uGrMyJ-_WFP5qY9mYKpycWWuaVjgkQ&oe=64ACCFCF",
+        },
+
+        
+
+
+    },
+
+
+    
+    {
+        id: 9,
+        image: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/358153953_286059867415675_886943238374452285_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeF6g2gOSiZuMwGr3-KUCVRLsefBwfQ5s7yx58HB9DmzvC3oNHPggJZvSqWEXwPONKIdVm3Us9WA4tDtSVt-hGvE&_nc_ohc=Dxx3AQaJkasAX_8FEiH&_nc_ht=scontent.fdac31-1.fna&oh=00_AfDlTbH_HXU9CrnXxQP29qjKHjXjzJ4GjNua5mVmZ83hsw&oe=64AD68BC",
+        contet:`যদি তুমি বলো যে কি দেখে আমায় তুমি ভালোবেসে ছিলে, তাহলে আমি বলবো তোমার ওই কেশবতী বেণী যাতে আমি ইচ্ছে মতো ফুল গুঁজে দিতে পারি।❤️🌼`,
+        like: 500,
+        commet :30,
+        timeAgo:"40 Min",
+        author :{
+            name: "Sorry Priyo - সরি প্রিয়",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/275673024_127825429811400_74803478024524733_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGbOVgkkp2lNIeVJKU72pWiFIMj5foxMZ4UgyPl-jExnk8ODmO7tl-R4PQrpFk1Owy44XFWsjob94QwM-3-RfO6&_nc_ohc=eeB-uwXA3gwAX_pzm4s&_nc_ht=scontent.fdac31-1.fna&oh=00_AfA3ZU-5nzwHoLeRqybIeD0BQ0m5u9WrCB8n9uOjc59R2w&oe=64ACE0F7",
+        },
+
+        
+
+    },
+
+
+    
+    
+    {
+        id: 1,
+        image: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/358094767_811572873978074_625345031696052425_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeHCN4umYLCqnEZfIoZ2RAtYRxgMVjDT90VHGAxWMNP3RatA5oZPQoniPQm7zifTXCrYFWItmR_eb5MNRfM-pKRU&_nc_ohc=FMMY6_hoPuUAX8x2FCR&_nc_ht=scontent.fdac31-1.fna&oh=00_AfBPW_BdufPwSSZJWykBMryDco_rINN5AcXW7YKCIH0y9A&oe=64ACACCF",
+        contet:`তিনি আছেন এবং শেষ পর্যন্ত তিনিই থাকবেন♥️`,
+        like: 500,
+        commet :30,
+        timeAgo:"40 Min",
+        author :{
+            name: "Kaynat Rahaman",
+            photo: "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/275673024_127825429811400_74803478024524733_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGbOVgkkp2lNIeVJKU72pWiFIMj5foxMZ4UgyPl-jExnk8ODmO7tl-R4PQrpFk1Owy44XFWsjob94QwM-3-RfO6&_nc_ohc=eeB-uwXA3gwAX_pzm4s&_nc_ht=scontent.fdac31-1.fna&oh=00_AfA3ZU-5nzwHoLeRqybIeD0BQ0m5u9WrCB8n9uOjc59R2w&oe=64ACE0F7",
+        },
+
+        
+
+    },
+
+
+
+
+
+
+
+
+]
